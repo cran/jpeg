0b1be768c38d4466322e27847d74ebe4e800de7d
Simon Urbanek <Simon.Urbanek@r-project.org>
2012-01-31 00:00:00 +0000

    version 0.1-2

--- R/write.R
+++ R/write.R
@@ -1,2 +1,8 @@
-writeJPEG <- function(image, target, quality = 0.7, bg = "white")
-  invisible(.Call("write_jpeg", image, if (is.raw(target)) target else path.expand(target), quality, bg, PACKAGE="jpeg"))
+writeJPEG <- function(image, target = raw(), quality = 0.7, bg = "white", color.space) {
+  if (missing(color.space)) color.space <- attr(image, "color.space")
+  if (inherits(target, "connection")) {
+    r <- .Call("write_jpeg", image, raw(), quality, bg, color.space, PACKAGE="jpeg")
+    writeBin(r, target)
+    invisible(NULL)
+  } else invisible(.Call("write_jpeg", image, if (is.raw(target)) target else path.expand(target), quality, bg, color.space, PACKAGE="jpeg"))
+}
